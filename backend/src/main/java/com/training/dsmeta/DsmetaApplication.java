18c155160821a33df4c17ad95b38a0d890725be4
daviwillpereira <daviwillpereira@gmail.com>
2022-11-16 11:07:43 -0300

    Project created

---
+++ backend/src/main/java/com/training/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.training.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
